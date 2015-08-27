f1cbac41eff9ba8e8442a0cbf4d4445d5bac6394
kojiagile <cowboyjpn1919@gmail.com>
2015-08-28 00:35:34 +1000

    Add Fence class
    Add Fence class by Koji

---
+++ backend/src/main/java/ifn372/sevencolors/backend/entities/Fence.java
@@ -0,0 +1,83 @@
+package ifn372.sevencolors.backend.entities;
+
+/**
+ * Created by koji on 2015/08/28.
+ */
+public class Fence {
+
+    //Return value (creation successful/fail)
+    boolean success;
+
+    String userId;
+    String fenceName;
+    double lon;
+    double lat;
+    float radius;
+    String address;
+
+    public Fence() {
+    }
+
+    public Fence(boolean success) {
+        this.success = success;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getFenceName() {
+        return fenceName;
+    }
+
+    public void setFenceName(String fenceName) {
+        this.fenceName = fenceName;
+    }
+
+    public double getLon() {
+        return lon;
+    }
+
+    public void setLon(double lon) {
+        this.lon = lon;
+    }
+
+    public double getLat() {
+        return lat;
+    }
+
+    public void setLat(double lat) {
+        this.lat = lat;
+    }
+
+    public float getRadius() {
+        return radius;
+    }
+
+    public void setRadius(float radius) {
+        this.radius = radius;
+    }
+
+
+}
